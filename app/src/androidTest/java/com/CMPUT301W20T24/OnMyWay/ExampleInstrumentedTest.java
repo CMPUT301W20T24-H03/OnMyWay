203944248c20980a0d8a1c230f4ba89c7b0e665d
John Goodliff <johny112009@hotmail.com>
2020-02-17 15:20:55 -0700

    Rename package name

--- app/src/androidTest/java/com/CMPUT301W20T24/OnMyWay/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/CMPUT301W20T24/OnMyWay/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.onmyway;
+package com.CMPUT301W20T24.OnMyWay;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.onmyway", appContext.getPackageName());
+        assertEquals("com.CMPUT301W20T24.OnMyWay", appContext.getPackageName());
     }
 }
