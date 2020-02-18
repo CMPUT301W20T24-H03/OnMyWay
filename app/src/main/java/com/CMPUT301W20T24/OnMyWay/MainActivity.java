bb4c484b05388f7c5d906653ec504f6ae9922478
John Goodliff <johny112009@hotmail.com>
2020-02-17 21:21:00 -0700

    Create SplashScreenActivity. Add button for SplashScreenActivity in MainActivity

--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/MainActivity.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/MainActivity.java
@@ -1,13 +1,23 @@
 package com.CMPUT301W20T24.OnMyWay;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    // Called when the user presses a button
+    public void openSplashScreenActivity(View view) {
+        Intent intent = new Intent(this, SplashScreenActivity.class);
+        startActivity(intent);
+    }
+
 }
