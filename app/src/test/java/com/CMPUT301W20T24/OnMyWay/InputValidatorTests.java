b05528faebc7181f256dbdf3bace506688451c1e
John Goodliff <johny112009@hotmail.com>
2020-02-28 19:00:53 -0700

    Add tests for InputValidatorResponse class

---
+++ app/src/test/java/com/CMPUT301W20T24/OnMyWay/InputValidatorTests.java
@@ -0,0 +1,19 @@
+package com.CMPUT301W20T24.OnMyWay;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InputValidatorTests {
+    @Test
+    void testInputValidatorResponse() {
+        String errorName = "Error name";
+        InputValidatorResponse successResponse = new InputValidatorResponse();
+        InputValidatorResponse failureResponse = new InputValidatorResponse(errorName);
+
+        assertTrue(successResponse.success());
+        assertFalse(failureResponse.success());
+
+        assertEquals(errorName, failureResponse.getErrorMsg());
+        assertThrows(UnsupportedOperationException.class, successResponse::getErrorMsg);
+    }
+}
