95046da23229f732a2a820cf7f3fc04fb1988454
indietogger <mdoshi@ualberta.ca>
2020-03-05 11:49:54 -0700

    a

--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/LoginActivity.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
     private static final String TAG = "OMW/LoginActivity";   // Use this tag for call Log.d()
     private EditText emailField;
     private EditText passwordField;
--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/SignUpActivity.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/SignUpActivity.java
@@ -1,10 +1,20 @@
 package com.CMPUT301W20T24.OnMyWay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,9 +23,13 @@ public class SignUpActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
 
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
 
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
@@ -27,7 +41,10 @@ public class SignUpActivity extends AppCompatActivity {
                         .setAvailableProviders(providers)
                         .build(),
                 RC_SIGN_IN);
+
     }
+
+
 
 }
 
