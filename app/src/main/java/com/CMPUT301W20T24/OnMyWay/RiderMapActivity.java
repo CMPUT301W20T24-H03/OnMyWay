b4fcde49d054cdbb1391504d20f8facc875c851a
mgrewal12 <60236560+mgrewal12@users.noreply.github.com>
2020-03-10 20:08:44 -0600

    Updated the RiderMap xml document to include a search bar, and activated that search bar in the Java class file. Need to finish error handling, grabbing only two locations, and updating those in Firebase.

--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/RiderMapActivity.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/RiderMapActivity.java
@@ -1,104 +1,68 @@
 package com.CMPUT301W20T24.OnMyWay;
 
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import android.widget.SearchView;
+
+import androidx.fragment.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
 
 public class RiderMapActivity extends FragmentActivity implements OnMapReadyCallback {
-    private static final String TAG = "OMW/RiderMapActivity";
-    private GoogleMap mMap;
-
-    /// Android Coding via YouTube, How to Show Current Location On Map in Android Studio
-    /// https://www.youtube.com/watch?v=boyyLhXAZAQ&t=22s
-    Location currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    View mapView;
-    private static final int REQUEST_CODE = 101;
-
-    // HACK TO GO BACK TO THE MAIN ACTIVITY WHEN THE BACK BUTTON IS PRESSED. REMOVE LATER
-    @Override
-    public void onBackPressed() {
-        Log.d(TAG, "Switching to MainActivity");
-        Intent intent = new Intent(RiderMapActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
+    GoogleMap map;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_map);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLastLocation();
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        Toast.makeText(RiderMapActivity.this, "RiderMapActivity", Toast.LENGTH_LONG).show();
-    }
+        searchView = findViewById(R.id.locationSearchBar);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.riderMap);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-    private void fetchLastLocation() {
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-        }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onSuccess(Location location) {
-                if(location!=null){
-                    currentLocation = location;
-                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + " " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-                    mapView = mapFragment.getView();
-                    mapFragment.getMapAsync(RiderMapActivity.this);
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = searchView.getQuery().toString();
+                List<Address> addressList;
+
+                if (!location.equals("")) {
+                    Geocoder geocoder = new Geocoder(RiderMapActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        map.addMarker(new MarkerOptions().position(latLng).title(location));
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
-
-        // Add a marker in Sydney and move the camera
-        LatLng current_coordinates = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(current_coordinates).title("Marker at current location (RIDER)"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(current_coordinates));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current_coordinates,15));
-
-        /// Stack Overflow post by Adam https://stackoverflow.com/users/6789978/adam
-        /// Answer https://stackoverflow.com/questions/36785542/how-to-change-the-position-of-my-location-button-in-google-maps-using-android-st
-        View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
-        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
-        // position on right bottom
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);rlp.setMargins(30,30,30,120);
+        map = googleMap;
     }
 }
