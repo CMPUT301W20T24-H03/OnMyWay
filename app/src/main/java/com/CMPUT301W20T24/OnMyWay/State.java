1b0e9400f9ab28641aa8e5144d5f7cd7a6f39f1c
John Goodliff <johny112009@hotmail.com>
2020-03-12 22:10:17 -0600

    Add comments to State

--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/State.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/State.java
@@ -1,22 +1,41 @@
 package com.CMPUT301W20T24.OnMyWay;
 
 
+/**
+ * Stores objects needed for the global state of the application
+ * (objects shared between multiple activities, classes in the app simultaneously like
+ * current user, current request, etc.). Has methods for updating values in the State.
+ * Later this will be modified to either fetch data from the database or offline,
+ * depending on whether the user has an internet connection or not
+ * @author John
+ */
 // Keeps track of the main application state. It holds the current user for now
-// Should probably save this offline
+// Will modify this to save offline later
 public class State {
     private static final String TAG = "OMW/State";   // Use this tag for call Log.d()
     static private User currentUser;
+    // Make a DBManager so we can interact with the database
     static private DBManager dbManager = new DBManager();
 
 
-    // This will be null if user doesn't exist
+    /**
+     * Returns the currently logged in user. This will be null if the user isn't logged in yet
+     * @return A User object of the currently logged in User
+     * @author John
+     */
+    // TODO: Return a ResponseStatus instead so activities can call SplashScreen if the current
+    // TODO: user doesn't exist for whatever reason
     static public User getCurrentUser() {
         return currentUser;
     }
 
 
-    // Set current user only if there isn't one already.
-    // May have to change this if we save data offline
+    /**
+     * Sets the current user only if one doesn't exist already
+     * @param newUser The User object we want to change currentUser to
+     * @author John
+     */
+    // TODO: Modify to allow data to be saved offline
     static public void setCurrentUser(User newUser) {
         if (currentUser == null) {
             currentUser = newUser;
@@ -25,23 +44,41 @@ public class State {
     }
 
 
-    // Calls DBManager to push new user info to FireStore
+    /**
+     * Calls DBManager to push user info to FireStore. To modify currentUser,
+     * use getCurrentUser() first to get and modify the current User, then call this method to push
+     * the changes to the database
+     * @author John
+     */
     static public void updateCurrentUser() {
         dbManager.pushUserInfo(getCurrentUser());
     }
 
 
-    // Checks if there is a user logged in
+    /**
+     * Checks if there is a user logged in or not
+     * @return A boolean indicating whether the user is logged in or not
+     * @author John
+     */
     static public boolean isLoggedIn() {
         return dbManager.getFirebaseUser() != null;
     }
 
 
+    /**
+     * A private method to remove currentUser (used to cleanup before logging out, for example)
+     * @author John
+     */
     static private void removeCurrentUser() {
         currentUser = null;
     }
 
 
+    /**
+     * Logs out the current user. First removes the current user from State and then calls
+     * DBManager to log out the user from Firebase Auth
+     * @author John
+     */
     // This function works. Don't know if this should be here or in State
     static public void logoutUser() {
         removeCurrentUser();
