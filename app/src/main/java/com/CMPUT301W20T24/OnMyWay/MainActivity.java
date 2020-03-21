4a67efa5e21b6838dd6b32e23b627b4fd2e56fdb
John Goodliff <johny112009@hotmail.com>
2020-03-20 19:42:56 -0600

    Remove login and logout buttons because they break normal app flow. Use splash screen and view current profile instead

--- app/src/main/java/com/CMPUT301W20T24/OnMyWay/MainActivity.java
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/MainActivity.java
@@ -51,13 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     // Called when the user presses a button
-    public void openLoginActivity(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-
-    // Called when the user presses a button
     public void openMainDriverMapActivity(View view) {
         Intent intent = new Intent(this, DriverMapActivity.class);
         startActivity(intent);
@@ -123,21 +116,5 @@ public class MainActivity extends AppCompatActivity {
     public void openRiderCostActivity(View view) {
         Intent intent = new Intent(this, RiderCostActivity.class);
         startActivity(intent);
-    }
-
-    // Called when the user presses a button
-    public void logout(View view) {
-        String msg = "Logged out";
-
-        State.logoutUser();
-        Intent intent = new Intent(this, SplashScreenActivity.class);
-        intent.putExtra("toastMessage", "Logged out successfully");
-        startActivity(intent);
-
-        Log.w(TAG, msg);
-        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-
-        statusTextCurrentUser.setText("None");
-        statusTextCurrentUser.setTextColor(ContextCompat.getColor(this, R.color.colorError));
     }
 }
