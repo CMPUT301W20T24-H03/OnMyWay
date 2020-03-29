7628b558ae45a07482cc872faacd03db9d6d399c
John Goodliff <johny112009@hotmail.com>
2020-03-28 20:46:00 -0600

    Add RequestTime class

---
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/RequestTime.java
@@ -0,0 +1,26 @@
+package com.CMPUT301W20T24.OnMyWay;
+
+import java.util.Calendar;
+
+import static java.lang.Math.abs;
+
+public class RequestTime {
+    private long secondsSinceEpoch;
+
+    public RequestTime() {
+        secondsSinceEpoch = Calendar.getInstance().getTime().getTime() / 1000;
+    }
+
+    public long toLong() {
+        return secondsSinceEpoch;
+    }
+
+    // Get absolute value of the difference between 2 times, formatted like mm:ss
+    public String getTimeDifference(RequestTime otherTime) {
+        long timeDiffSeconds = abs(secondsSinceEpoch - otherTime.toLong());
+        long minutes = timeDiffSeconds / 60;
+        long seconds = timeDiffSeconds % 60;
+
+        return minutes + ":" + seconds;
+    }
+}
