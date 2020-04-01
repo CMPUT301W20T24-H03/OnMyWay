bc1aeddcbb0946f58cb019a445ea2051a02a7bc1
Neel <neel1@ualberta.ca>
2020-03-31 20:23:14 -0600

    DriverMap further changes. Added history button, will implement

---
+++ app/src/main/java/com/CMPUT301W20T24/OnMyWay/DriverHistoryFragment.java
@@ -0,0 +1,36 @@
+package com.CMPUT301W20T24.OnMyWay;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class DriverHistoryFragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // Icon made from Google
+        View view = inflater.inflate(R.layout.driver_history_button, container, false);
+
+        User currentUser = UserRequestState.getCurrentUser();
+        view.setOnClickListener(this);
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        // TODO
+    }
+}
